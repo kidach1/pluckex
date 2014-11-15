06b58788f34a05a806453c91be4c787c89bbdd10
Daiki Taniguchi <daiki.taniguchi@aktsk.jp>
2014-11-15 15:28:51 +0900

    add spec

--- spec/lib/pluckex_spec.rb
+++ spec/lib/pluckex_spec.rb
@@ -1,11 +1,25 @@
 require 'spec_helper'
 
 describe Pluckex do
-  it 'has a version number' do
-    expect(Pluckex::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe 'activerecord extention' do
+    let(:people) { Person.all }
+    describe 'pluck_tied_by_id' do
+      subject { Person.pluck_tied_by_id(:name) }
+      it do
+        expect(subject).to eq(
+          people.first.id => people.first.name,
+          people.second.id => people.second.name,
+        )
+      end
+    end
+    describe 'pluck_with_keys' do
+      subject { Person.pluck_with_keys(:id, :name) }
+      it do
+        expect(subject).to eq([
+          {:id => people.first.id, :name => people.first.name},
+          {:id => people.second.id, :name => people.second.name},
+        ])
+      end
+    end
   end
 end
