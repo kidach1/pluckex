2c025085019cec01c8948a15c85e48da237ae9d3
Daiki Taniguchi <daiki.taniguchi@aktsk.jp>
2014-11-15 15:28:15 +0900

    ready for spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,11 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'pluckex'
+
+ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => File.dirname(__FILE__) + "/pluckex.sqlite3"
+)
+
+load File.dirname(__FILE__) + '/support/schema.rb'
+load File.dirname(__FILE__) + '/support/models.rb'
+load File.dirname(__FILE__) + '/support/data.rb'
---
+++ spec/support/data.rb
@@ -0,0 +1,2 @@
+Person.create(:name => "kidach1", :age => 25)
+Person.create(:name => "bob", :age => 27)
